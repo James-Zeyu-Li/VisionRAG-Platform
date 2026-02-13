19a6def317e97cf60a73b66cc389028d1d750e77
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2026-02-13 14:55:02 -0800

    update on Config, add Vue Frontend

--- PublicServiceGo/router/router.go
+++ PublicServiceGo/router/router.go
@@ -1,16 +1,27 @@
 package router
 
 import (
+	"VisionRAG/PublicServiceGo/gateway"
+	"os"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 
 	r := gin.Default()
+	
+	// 注册用户相关本地路由
 	enterRouter := r.Group("/api/v1")
 	{
 		RegisterUserRouter(enterRouter.Group("/user"))
 	}
+
+	// 注册网关转发路由，转发到 ChatServiceGo
+	chatServiceAddr := os.Getenv("CHAT_SERVICE_ADDR")
+	if chatServiceAddr == "" {
+		chatServiceAddr = "http://localhost:9092"
+	}
+	gateway.RegisterGatewayRoutes(r, chatServiceAddr)
 
 	return r
 }
